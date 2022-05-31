6096e3cd92b0df31487363faf26a895236727fff
charlotte-2222 <ayy.charlotte@gmail.com>
2022-05-31 10:49:46 -0400

    Updating

--- app/src/main/java/com/example/charlotte_childers_cpt_188_lab_2/MainActivity.kt
+++ app/src/main/java/com/example/charlotte_childers_cpt_188_lab_2/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.charlotte_childers_cpt_188_lab_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    //Vars
+    private lateinit var trueBtn:Button
+    private lateinit var falseBtn:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        trueBtn=findViewById(R.id.true_button)
+        falseBtn=findViewById(R.id.false_button)
+
+
+        //methods
+        trueBtn.setOnClickListener{view:View ->
+         Toast.makeText(
+             this,
+             R.string.correct_toast,
+             Toast.LENGTH_SHORT
+         ).show()
+        }
+        falseBtn.setOnClickListener{view:View ->
+        Toast.makeText(
+            this,
+            R.string.incorrect_toast,
+            Toast.LENGTH_SHORT
+        ).show()
+        }
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/charlotte_childers_cpt_188_lab_2/Question.kt
@@ -0,0 +1,7 @@
+package com.example.charlotte_childers_cpt_188_lab_2
+
+import androidx.annotation.StringRes
+
+data class Question(@StringRes val texResId:Int, val answer:Boolean) {
+
+}
